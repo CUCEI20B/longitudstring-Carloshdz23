e7c62f0680bf3f3b80a3a14f17065531a5637e69
Carlos Hernández <cdavid.hernandez@academicos.udg.mx>
2020-10-05 23:41:05 -0500

    sizeof()

--- main.cpp
+++ main.cpp
@@ -5,9 +5,53 @@ using namespace std;
 int main() {
     string cadena;
 
-    // cin >> cadena; // lee hasta el espacio
-    getline(cin, cadena); // lee hasta \n
+    cin >> cadena;
     cout << cadena.size() << endl;
+
+    if(cadena == "char")
+    {
+        cout << sizeof(char) << endl;
+    }
+
+    cin >> cadena;
+    cout << cadena.size() << endl;
+
+    if(cadena == "int")
+    {
+        cout << sizeof(int) << endl;
+    }
+
+    cin >> cadena;
+    cout << cadena.size() << endl;
+
+    if(cadena == "short")
+    {
+        cout << sizeof(short) << endl;
+    }
+
+    cin >> cadena;
+    cout << cadena.size() << endl;
+
+    if(cadena == "float")
+    {
+        cout << sizeof(float) << endl;
+    }
+
+    cin >> cadena;
+    cout << cadena.size() << endl;
+
+    if(cadena == "double")
+    {
+        cout << sizeof(double) << endl;
+    }
+
+    cin >> cadena;
+    cout << cadena.size() << endl;
+
+    if(cadena == "long")
+    {
+        cout << sizeof(long) << endl;
+    }
 
     return 0;
 }
